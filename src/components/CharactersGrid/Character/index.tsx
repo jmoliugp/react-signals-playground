a522432bad56fb6c0cdbce0844dcb535b8172241
Solo <juan@xmartlabs.com>
2023-11-24 16:45:16 -0300

    splits grid components

---
+++ src/components/CharactersGrid/Character/index.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+
+import { Affiliation, Character } from "../../../entities/character";
+import "./index.css";
+import jediIcon from "/jediIcon.png";
+import sithIcon from "/sithLogo.png";
+import unknownAvatar from "/unknownAvatar.png";
+import { Profiler } from "../../Profiler";
+
+export interface CharacterProps {
+  jedi?: Character;
+  sith?: Character;
+  sithCounter: number;
+  id: number;
+}
+
+const useCharacter = ({ id, sithCounter, jedi, sith }: CharacterProps) => {
+  const [character, setCharacter] = useState<Character | undefined>(undefined);
+
+  const minDelay = 100;
+  const maxDelay = 4000;
+  const delay =
+    Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+
+  useEffect(() => {
+    const currCharacter = id <= sithCounter ? sith : jedi;
+    const isNewCharacter = currCharacter !== character;
+    if (isNewCharacter) {
+      setCharacter(undefined);
+    }
+
+    setTimeout(() => {
+      // setCharacter(undefined);
+      const newCharacter = id <= sithCounter ? sith : jedi;
+      setCharacter(newCharacter);
+    }, delay);
+  }, [id, sithCounter, jedi, sith, delay, character]);
+
+  return character;
+};
+
+const expensiveCalculation = () => {
+  const minDelay = 100;
+  const maxDelay = 400;
+  const delay =
+    Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+
+  const start = Date.now();
+  while (Date.now() - start < delay) {
+    // Loop to force a delay.
+  }
+};
+
+export const CharacterSwapi: React.FC<CharacterProps> = (props) => {
+  const character = useCharacter(props);
+  expensiveCalculation();
+
+  const imageSrc = character
+    ? `./swapiCharacters/${character.id}.jpg`
+    : unknownAvatar;
+  const title = character ? character.name : "Loading...";
+
+  return (
+    <Profiler id="Character">
+      <div key={props.id} className="gridItem">
+        <div className="image-container">
+          <img className="character-avatar" src={imageSrc} alt={title} />
+          {character && (
+            <img
+              className="affiliation-logo"
+              src={
+                character.affiliation === Affiliation.Jedi ? jediIcon : sithIcon
+              }
+              alt="Affiliation Logo"
+            />
+          )}
+        </div>
+        <h2 className="character-name">{title}</h2>
+      </div>
+    </Profiler>
+  );
+};
---
+++ src/components/CharactersGrid/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Character } from "../../entities/character";
+import { CharacterProps, CharacterSwapi } from "./Character";
+import "./index.css";
+
+interface GridSwapiCharactersProps {
+  jedis: Character[];
+  siths: Character[];
+  sithCounter: number;
+}
+
+export const CharactersGrid: React.FC<GridSwapiCharactersProps> = ({
+  jedis,
+  siths,
+  sithCounter,
+}) => {
+  const GRID_SIZE = 6;
+  const gridItems: CharacterProps[] = Array.from(
+    { length: GRID_SIZE * GRID_SIZE },
+    (_, index) => {
+      const jedi = jedis[index % jedis.length];
+      const sith = siths[index % siths.length];
+
+      const id = index + 1;
+
+      return { id, jedi, sith, sithCounter };
+    }
+  );
+
+  return (
+    <div className="gridContainer">
+      {gridItems.map((props) => (
+        <CharacterSwapi key={props.id} {...props} />
+      ))}
+    </div>
+  );
+};
